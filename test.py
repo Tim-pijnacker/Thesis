14d342bdeab02546351d6f784affd06521c5f108
tim-pijnacker <tim.pijnacker@hotmail.com>
2022-11-17 19:01:11 +0100

    nsection c++ update

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import torch
 import nsection_cpp
+from torch.autograd import Function
+from nsection_bench import EntmaxNsectFunction
 
 
 def p(Z, tau, alpha):
@@ -7,10 +9,24 @@ def p(Z, tau, alpha):
     Z = Z ** (1 / (alpha - 1))
     return Z
 
-Z = torch.randn(4)
-tau = torch.randn(4)
-a = [1.5]*4
-alpha = torch.Tensor(a)
-output = nsection_cpp.forward(Z, tau, alpha)
-print(output)
-print(p(Z, tau, alpha))
+def sctn_check(X, tau_lo, tau_hi, alpha, dim):
+    f_lo = p(X, tau_lo, alpha)
+    f_lo = f_lo.sum(dim) - 1
+    f_hi = p(X, tau_hi, alpha)
+    f_hi = f_hi.sum(dim) - 1
+    out = ((f_lo > 0) & (f_hi <= 0)).unsqueeze(dim)
+    return out
+
+# test input
+Z = torch.randn(10,5)
+dim = -1
+
+# test tau lo and hi
+tau_lo = torch.randn(10,5)-1
+tau_hi = tau_lo+2
+
+# test alpha
+alpha = 1.5
+
+print(EntmaxNsectFunction.forward(Function, Z, alpha))
+print(nsection_cpp.forward(Z, tau_lo, alpha, dim))
