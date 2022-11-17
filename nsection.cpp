9b9239c99d94e5aa1f6c06bbbef556ea9f4ee2eb
tim-pijnacker <tim.pijnacker@hotmail.com>
2022-11-17 19:01:00 +0100

    nsection c++ update

--- nsection.cpp
+++ nsection.cpp
@@ -3,15 +3,6 @@
 #include <iostream>
 
 
-// def sctn_check(cls, X, tau_lo: float, tau_hi: float, alpha: float, dim):
-//     f_lo = cls.p(X, tau_lo, alpha)
-//     f_lo = f_lo.sum(dim) - 1
-//     f_hi = cls.p(X, tau_hi, alpha)
-//     f_hi = f_hi.sum(dim) - 1
-//     out = ((f_lo > 0) & (f_hi <= 0)).unsqueeze(dim)
-//     return out
-
-
 torch::Tensor prob(
     torch::Tensor Z,
     torch::Tensor tau,
@@ -27,22 +18,53 @@ torch::Tensor prob(
 
 torch::Tensor sctn_check(
     torch::Tensor Z,
-    torch::Tensor tau_lo,
-    torch::Tensor tau_hi,
+    torch::Tensor tauLo,
+    torch::Tensor tauHi,
     torch::Tensor alpha,
-    torch::Scalar dim
-){}
+    int dim
+){
+    torch::Tensor fLo = prob(Z, tauLo, alpha);
+    fLo = torch::sum(fLo,dim) - 1;
+    torch::Tensor fHi = prob(Z, tauHi, alpha);
+    fHi = torch::sum(fHi,dim) - 1;
+    torch::Tensor mask = torch::__and__((fLo > 0), (fHi <= 0));
+    torch::Tensor out = torch::unsqueeze(mask, dim);
+    return out;
+}
 
 
 torch::Tensor nsection_forward(
     torch::Tensor Z,
     torch::Tensor tau,
-    torch::Tensor alpha 
+    float alphaFlt,
+    int nIters
+    int dim
 ){
-    torch::Tensor output = prob(Z, tau, alpha);
-    return output;
+    // create alpha tensor
+    torch::Tensor shape = torch::_shape_as_tensor(Z);
+    int d = shape[1].item<int>();
+    shape[dim] = 1;
+    torch::Tensor alpha = torch::ones({shape[0].item<int>(), shape[1].item<int>()}) + (alphaFlt - 1);
+
+    // calculate max 
+    torch::Tensor max = torch::amax(Z, dim, true);
+    max = max * (alphaFlt - 1);
+    Z = Z * (alphaFlt - 1);
+
+    // calculate starting tau high and low
+    torch::Tensor tauLo = max - 1;
+    float factor = 1 / (float) d; 
+    torch::Tensor power = alpha - 1;
+    torch::Tensor tauHi = max - torch::float_power(factor, power);
+    
+    torch::Tensor sctnRng = tauHi - tauLo;
+    for(int i = 0, i < nIter, i++){
+
+    }
+    return sctnRng;
 }
 
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m){
     m.def("forward", &nsection_forward, "Nsection forward");
+    m.def("sctn_check", &sctn_check, "section check");
 }
