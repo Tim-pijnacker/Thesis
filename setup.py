b0c2811aa5e352417ffdf22862b72a9edebf2e7e
Tim-pijnacker <tim.pijnacker@hotmail.com>
2022-11-17 14:21:11 +0100

    new setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, Extension
 from torch.utils import cpp_extension
 
-setup(name='hello_cpp',
-      ext_modules=[cpp_extension.CppExtension('hello_cpp', ['hello.cpp'])],
+setup(name='nsection_cpp',
+      ext_modules=[cpp_extension.CppExtension('nsection_cpp', ['nsection.cpp'])],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
