63215968efa48b2fe22ba39da17324c4c9bc0587
Tim-pijnacker <tim.pijnacker@hotmail.com>
2022-11-23 12:54:58 +0100

    faster cpp implementation

--- cpp/nsection.cpp
+++ cpp/nsection.cpp
@@ -6,12 +6,10 @@
 torch::Tensor prob(
     torch::Tensor Z,
     torch::Tensor tau,
-    torch::Tensor alpha    
+    float alpha    
 ){
-    torch::Scalar min = 0;
-    Z = torch::clamp_min(Z - tau, min);
-    torch::Tensor power = 1 / (alpha - 1);
-    Z = torch::float_power(Z, power);
+    Z = torch::clamp_min(Z - tau, 0);
+    Z = torch::float_power(Z, (1 / (alpha - 1)));
     return Z;
 }
 
@@ -20,50 +18,50 @@ torch::Tensor sctn_check(
     torch::Tensor Z,
     torch::Tensor tauLo,
     torch::Tensor tauHi,
-    torch::Tensor alpha,
+    float alpha,
     int dim
 ){
-    torch::Tensor fLo = prob(Z, tauLo, alpha);
+    auto fLo = prob(Z, tauLo, alpha);
     fLo = torch::sum(fLo,dim) - 1;
-    torch::Tensor fHi = prob(Z, tauHi, alpha);
+    auto fHi = prob(Z, tauHi, alpha);
     fHi = torch::sum(fHi,dim) - 1;
-    torch::Tensor mask = torch::__and__((fLo > 0), (fHi <= 0));
-    torch::Tensor out = torch::unsqueeze(mask, dim);
+    auto mask = torch::__and__((fLo > 0), (fHi <= 0));
+    auto out = torch::unsqueeze(mask, dim);
     return out;
 }
 
 
 torch::Tensor nsection_forward(
     torch::Tensor Z,
-    torch::Tensor alpha,
-    float alphaVal,
+    float alpha,
     int dim,
     int nIters,
     int nSections,
     bool ensure_sum_one    
 ){
-    torch::Tensor shape = torch::_shape_as_tensor(Z);
-    int d = shape[1].item<int>();
+    auto shape = torch::_shape_as_tensor(Z);
+    auto d = shape[1].item<int>();
 
     // calculate max 
-    torch::Tensor max = torch::amax(Z, dim, true);
-    max = max * (alphaVal - 1);
-    Z = Z * (alphaVal - 1);
+    auto max = torch::amax(Z, dim, true);
+    max = max * (alpha - 1);
+    Z = Z * (alpha - 1);
 
     // calculate starting tau high and low
-    torch::Tensor tauLo = max - 1;
-    double factor = 1 / (double) d; 
-    torch::Tensor power = alpha - 1;
-    torch::Tensor tauHi = max - torch::float_power(factor, power);
+    auto tauLo = max - 1;
+    auto tauHi = max - pow(1 / (double) d, alpha - 1);
 
-    torch::Tensor sctnRng = tauHi - tauLo;
+    auto sctnRng = tauHi - tauLo;
+    auto sctnTauLo = torch::zeros_like(tauLo);
+    auto sctnTauHi = torch::zeros_like(tauLo);
+    // input_mat_cuda = torch:zeros_like(...)
     for(int i = 0; i < nIters; i++){
         sctnRng /= nSections;
         for(int j = 1; j < nSections+1; j++){
-            torch::Tensor sctnTauLo = tauLo + (j-1)*sctnRng;
-            torch::Tensor sctnTauHi = tauLo + j*sctnRng;
-            torch::Tensor mask = sctn_check(Z, sctnTauLo, sctnTauHi, alpha, dim);
-            tauLo = torch::where(mask, sctnTauLo, tauLo);
+            sctnTauLo = tauLo + (j-1)*sctnRng;
+            sctnTauHi = tauLo + j*sctnRng;
+            auto mask = sctn_check(Z, sctnTauLo, sctnTauHi, alpha, dim);
+            tauLo = torch::where_out(tauLo, mask, sctnTauLo, tauLo);
         } 
     }
     torch::Tensor p = prob(Z, tauLo, alpha);
@@ -79,9 +77,9 @@ torch::Tensor sparsemax_backward(
     torch::Tensor dY,
     int dim
 ){
-    torch::Tensor gppr = torch::where(Y > 0, 1.0, 0.0);
-    torch::Tensor dX = dY * gppr;
-    torch::Tensor q =  torch::sum(dX,dim) / torch::sum(gppr,dim);
+    auto gppr = torch::where(Y > 0, 1.0, 0.0);
+    auto dX = dY * gppr;
+    auto q =  torch::sum(dX,dim) / torch::sum(gppr,dim);
     q = torch::unsqueeze(q, dim);
     dX -= q * gppr;
     return dX;
@@ -94,28 +92,12 @@ torch::Tensor entmax_backward(
     torch::Tensor alpha,
     int dim
 ){
-    torch::Tensor gppr = torch::where(Y > 0, torch::float_power(Y, (2 - alpha)), torch::zeros({1}));
-    torch::Tensor dX = dY * gppr;
-    torch::Tensor q =  torch::sum(dX,dim) / torch::sum(gppr,dim);
+    auto gppr = torch::where(Y > 0, torch::float_power(Y, (2 - alpha)), torch::zeros({1}));
+    auto dX = dY * gppr;
+    auto q =  torch::sum(dX,dim) / torch::sum(gppr,dim);
     q = torch::unsqueeze(q, dim);
     dX -= q * gppr;
     return dX;
-}
-
-torch::Tensor alpha_gradient(
-    torch::Tensor Y,
-    torch::Tensor dY,
-    torch::Tensor alpha,
-    int dim
-){
-    torch::Tensor S = torch::where(Y > 0, Y * torch::log(Y), torch::zeros({1}));
-    torch::Tensor ent = torch::unsqueeze(torch::sum(S,dim), dim);
-    torch::Tensor gppr = torch::where(Y > 0, torch::float_power(Y, (2 - alpha)), torch::zeros({1}));
-    torch::Tensor Y_skewed = gppr / torch::unsqueeze(torch::sum(gppr,dim), dim);
-    torch::Tensor d_alpha =  dY * (Y - Y_skewed) / torch::float_power(alpha-1, 2);
-    d_alpha -= dY * (S - Y_skewed * ent) / (alpha - 1);
-    d_alpha = torch::unsqueeze(torch::sum(d_alpha,dim), dim);
-    return d_alpha;
 }
 
 
@@ -123,5 +105,4 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m){
     m.def("forward", &nsection_forward, "Nsection forward");
     m.def("sparsemax_backward", &sparsemax_backward, "Sparsemax backward");
     m.def("entmax_backward", &entmax_backward, "Entmax backward");
-    m.def("alpha_gradient", &alpha_gradient, "Alpha gradient");
 }
