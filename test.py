901bf91b0424d25979d198551ba2efd8a081fdec
Tim-pijnacker <tim.pijnacker@hotmail.com>
2022-11-23 13:00:01 +0100

    new time

--- test.py
+++ test.py
@@ -4,10 +4,13 @@ import numpy as np
 
 from cpp.nsection import entmax_nsect_cpp
 from py.nsection import entmax_nsect
+from entmax import entmax_bisect
 from time import time
 
 # test input
 torch.manual_seed(42)
-Z = torch.randn(50,30000)
+Z = torch.randn(10,5)
 alpha = 1.5
 
+print(entmax_nsect_cpp(Z)[0])
+print(entmax_bisect(Z)[0])
