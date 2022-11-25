f088a08f8f2da42f7f41631f8ce309b41c2e1960
Tim-pijnacker <tim.pijnacker@hotmail.com>
2022-11-26 00:52:24 +0100

    cpp vectorized implementation

--- cpp/nsection.py
+++ cpp/nsection.py
@@ -7,11 +7,10 @@ import nsection_cpp
 
 class EntmaxNsectFunction(Function):
     @classmethod
-    def forward(cls, ctx, X, alpha=1.5, dim=-1, n_iter=5, n_sections=5):
+    def forward(cls, ctx, X, alpha=1.5, n_iter=5, n_sections=5):
         ctx.alpha = alpha
-        ctx.dim = dim
 
-        p_m = nsection_cpp.forward(X, alpha, dim, n_iter, n_sections)
+        p_m = nsection_cpp.forward(X, alpha, n_iter, n_sections)
 
         ctx.save_for_backward(p_m)
         return p_m
@@ -21,20 +20,19 @@ class EntmaxNsectFunction(Function):
         (Y,) = ctx.saved_tensors
 
         if ctx.alpha == 2:
-            dX = nsection_cpp.sparsemax_backward(Y, dY, ctx.dim)
+            dX = nsection_cpp.sparsemax_backward(Y, dY)
         else:
-            dX = nsection_cpp.entmax_backward(Y, dY, ctx.alpha, ctx.dim)
+            dX = nsection_cpp.entmax_backward(Y, dY, ctx.alpha)
 
         return dX, None, None, None, None, None
 
 
-def entmax_nsect_cpp(X, alpha=1.5, dim=-1, n_iter=5, n_sections=5):
-    return EntmaxNsectFunction.apply(X, alpha, dim, n_iter, n_sections)
+def entmax_nsect_cpp(X, alpha=1.5, n_iter=5, n_sections=5):
+    return EntmaxNsectFunction.apply(X, alpha, n_iter, n_sections)
 
 
 class EntmaxNsect(nn.Module):
-    def __init__(self, alpha=1.5, dim=-1, n_iter=5, n_sections=5):
-        self.dim = dim
+    def __init__(self, alpha=1.5, n_iter=5, n_sections=5):
         self.n_iter = n_iter
         self.alpha = alpha
         self.n_sections = n_sections
@@ -42,5 +40,5 @@ class EntmaxNsect(nn.Module):
 
     def forward(self, X):
         return entmax_nsect_cpp(
-            X, alpha=self.alpha, dim=self.dim, n_iter=self.n_iter, n_sections=self.n_sections
+            X, alpha=self.alpha, n_iter=self.n_iter, n_sections=self.n_sections
         )
