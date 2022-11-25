3a2fabd94a58b7916d60f65463007673ac75e8f2
Tim-pijnacker <tim.pijnacker@hotmail.com>
2022-11-26 00:54:01 +0100

    test

--- test.py
+++ test.py
@@ -2,15 +2,13 @@ import torch
 from torch.autograd import Function, grad
 import numpy as np
 
-from cpp.nsection import entmax_nsect_cpp
 from py.nsection import entmax_nsect
-from entmax import entmax_bisect
-from time import time
+import nsection_cpp
 
 # test input
 torch.manual_seed(42)
-Z = torch.randn(10,5)
+x = torch.randn(2,3)
 alpha = 1.5
 
-print(entmax_nsect_cpp(Z)[0])
-print(entmax_bisect(Z)[0])
+print(nsection_cpp.forward(x, alpha, 5, 5))
+print(entmax_nsect(x))
