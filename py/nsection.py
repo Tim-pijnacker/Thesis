d3a0b50131a97d6178f74b3c257bdd03c7253a6e
Tim-pijnacker <tim.pijnacker@hotmail.com>
2022-11-26 00:53:30 +0100

    small change for cuda input

--- py/nsection.py
+++ py/nsection.py
@@ -12,22 +12,21 @@ class EntmaxNsectFunction(Function):
 
         d = d ** (alpha - 1)
         tau_width = (d - 1) / d  
-        tau_frac = torch.linspace(0, 1, n_sections)
+        tau_frac = torch.linspace(0, 1, n_sections, device=x.device)
 
         for it in range(n_iter):
 
             # generate sections
-
             taus = tau_lo + tau_width * tau_frac
-
+            
             # compute all ps in one go
             ps = torch.clamp(x.unsqueeze(dim=-2) - taus.unsqueeze(dim=-1), min=0)
             ps = ps ** (alpha - 1)
             
             # compute normalization objective: will be decreasing.
             obj = ps.sum(dim=-1)
-
-            res = torch.searchsorted(-obj, -torch.ones(x.shape[:-1] + (1,)))
+            
+            res = torch.searchsorted(-obj, -torch.ones(x.shape[:-1] + (1,), device=x.device))
             res = res.squeeze()
 
             # tau_hi = taus[torch.arange(bsz), res]  # unnecessary
