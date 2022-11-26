4c7c6edc668a46acdd2d57d5d1f0a0f06f759cf2
Tim-pijnacker <tim.pijnacker@hotmail.com>
2022-11-26 15:12:15 +0100

    errors while building extension

--- cuda/nsection_cuda_kernel.cu
+++ cuda/nsection_cuda_kernel.cu
@@ -5,37 +5,17 @@
 
 #include <vector>
 
-template <typename scalar_t>
-__device__ __forceinline__ scalar_t prob(scalar_t z, scalar_t tau, scalar_t alpha) {
-    z = ((z - tau) < 0)?0:(z - tau);
-    // z = fmax((z - tau), 0);
-    const auto power = 1 / (alpha - 1);
-    return pow(z, power);
+#include <stdio.h>
+
+__global__ void helloFromGPU(void)
+{
+    printf("Hello World From GPU!\n");
 }
 
-template <typename scalar_t>
-__device__ __forceinline__ scalar_t sctn_check(
-    scalar_t z,
-    scalar_t tauLo, 
-    scalar_t tauHi, 
-    scalar_t alpha,
-    int dim
-){
-    // update fLo in global memory, and then take summ 
-    const auto fLo = prob(z, tauLo, alpha); 
-    // fLo = sum(fLo, dim) - 1; ??
-    const auto fHi = prob(z, tauHi, alpha);
-    // fHi = sum(fHi, dim) - 1 ??
-    return fLo;
-}
-
-torch::Tensor entmax_cuda_forward(
-    torch::Tensor Z,
-    float alpha,
-    int dim,
-    int nIters,
-    int nSections,
-    bool ensure_sum_one
-) {
-    return test;
+int test(void)
+{
+    printf("Hello World from CPU!\n");
+    helloFromGPU<<<1,10>>>();
+    cudaDeviceReset();
+    return 0;
 }
