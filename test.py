168bac930f75a948e6f7883e1b4bb87c85d388e3
Tim-pijnacker <tim.pijnacker@hotmail.com>
2022-12-05 22:50:37 +0100

    new test

--- test.py
+++ test.py
@@ -4,8 +4,9 @@ import pytest
 from python.nsection import entmax_nsect
 from cpp.nsection import entmax_nsect_cpp
 from entmax import entmax_bisect, sparsemax_bisect
+import nsection_cuda
 
-
+# pytest for gpu and cuda?
 @pytest.mark.parametrize("dtype", (torch.float32, torch.float64))
 @pytest.mark.parametrize("alpha", (1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3))
 def test_nsection_out(alpha, dtype):
@@ -22,3 +23,11 @@ def test_nsection_out(alpha, dtype):
 
     assert torch.sum((y_hat_py - y_true) ** 2) < 1e-7
     assert torch.sum((y_hat_cpp - y_true) ** 2) < 1e-7
+
+torch.manual_seed(42)
+x = torch.randn(5,10,device=torch.device("cuda:0"), dtype=torch.float32)
+out_cuda = nsection_cuda.forward(x, 1.5, 1, 5)
+out_py = entmax_nsect(x, 1.5, 1, 5)
+
+print(out_cuda)
+print(out_py)
