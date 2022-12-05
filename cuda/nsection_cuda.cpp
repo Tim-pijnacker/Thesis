3cb50564f10599c58f23baffee2a98c3f8f3e7fc
Tim-pijnacker <tim.pijnacker@hotmail.com>
2022-12-05 22:51:56 +0100

    current cuda implementation

--- cuda/nsection_cuda.cpp
+++ cuda/nsection_cuda.cpp
@@ -2,26 +2,29 @@
 #include <vector>
 
 // cuda declarations
-
-int test();
-// torch::Tensor entmax_cuda_forward();
+torch::Tensor entmax_cuda_forward(
+    torch::Tensor Z,
+    float alpha,
+    int nIters,
+    int nSections
+);
 // torch::Tensor entmax_cuda_backward();
 
-// #define CHECK_CUDA(x) TORCH_CHECK(x.device().is_cuda(), #x " must be a CUDA tensor")
-// #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
-// #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
+#define CHECK_CUDA(x) TORCH_CHECK(x.device().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
+#define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
-// torch::Tensor nsection_forward(
-//     torch::Tensor Z,
-//     float alpha,
-//     int nIters,
-//     int nSections
-// ){
-//     CHECK_INPUT(Z);
+torch::Tensor nsection_forward(
+    torch::Tensor Z,
+    float alpha,
+    int nIters,
+    int nSections
+){
+    CHECK_INPUT(Z);
 
-//     return entmax_cuda_forward(Z, alpha, nIters, nSections);
-// }
+    return entmax_cuda_forward(Z, alpha, nIters, nSections);
+}
 
-int test_func(void){
-    return test();
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m){
+    m.def("forward", &nsection_forward, "nsection forward");
 }
