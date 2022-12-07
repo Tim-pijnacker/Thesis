d962a83283e1902a3832902492f345d9ed2dae69
Tim-pijnacker <tim.pijnacker@hotmail.com>
2022-12-07 16:04:21 +0100

    cuda fix

--- cuda/nsection_cuda.cpp
+++ cuda/nsection_cuda.cpp
@@ -8,7 +8,6 @@ torch::Tensor entmax_cuda_forward(
     int nIters,
     int nSections
 );
-// torch::Tensor entmax_cuda_backward();
 
 #define CHECK_CUDA(x) TORCH_CHECK(x.device().is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
@@ -25,6 +24,34 @@ torch::Tensor nsection_forward(
     return entmax_cuda_forward(Z, alpha, nIters, nSections);
 }
 
+torch::Tensor sparsemax_backward(
+    torch::Tensor Y,
+    torch::Tensor dY
+){
+    auto gppr = torch::where(Y > 0, 1.0, 0.0);
+    auto dX = dY * gppr;
+    auto q =  torch::sum(dX,-1) / torch::sum(gppr, -1);
+    q = torch::unsqueeze(q, -1);
+    dX -= q * gppr;
+    return dX;
+}
+
+
+torch::Tensor entmax_backward(
+    torch::Tensor Y,
+    torch::Tensor dY,
+    float alpha
+){
+    auto gppr = torch::where(Y > 0, torch::float_power(Y, (2 - alpha)), torch::zeros({1}));
+    auto dX = dY * gppr;
+    auto q =  torch::sum(dX,-1) / torch::sum(gppr,-1);
+    q = torch::unsqueeze(q, -1);
+    dX -= q * gppr;
+    return dX;
+}
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m){
     m.def("forward", &nsection_forward, "nsection forward");
+    m.def("sparsemax_backward", &sparsemax_backward, "Sparsemax backward");
+    m.def("entmax_backward", &entmax_backward, "Entmax backward");
 }
