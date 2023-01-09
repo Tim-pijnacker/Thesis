a46cd0eb3e780775ae5b53fc6ed6e04adfc6ba37
Tim-pijnacker <tim.pijnacker@hotmail.com>
2023-01-09 20:01:37 +0100

    log base 2 axes

--- output_test.py
+++ output_test.py
@@ -2,7 +2,7 @@ import torch
 import pytest
 
 from python.nsection import entmax_nsect
-from cuda.nsection import entmax_nsect_cuda, entmax_nsect_cuda1
+from cuda.nsection import entmax_nsect_cuda
 from cpp.nsection import entmax_nsect_cpp
 from entmax import entmax_bisect
 
@@ -22,7 +22,6 @@ def test_nsection_out(alpha, dtype, device, dim):
     y_hat_cpp = entmax_nsect_cpp(x, alpha, n_iter=50, n_sections=2)
     if device == torch.device("cuda:0"):
         y_hat_cuda = entmax_nsect_cuda(x, alpha, n_iter=50, n_sections=2)
-        y_hat_cuda1 = entmax_nsect_cuda1(x, alpha, n_iter=50, n_sections=2)
 
     # python bisection entmax
     y_true = entmax_bisect(x, alpha)
@@ -31,7 +30,6 @@ def test_nsection_out(alpha, dtype, device, dim):
     assert torch.allclose(y_hat_cpp, y_true)
     if device == torch.device("cuda:0"):
         assert torch.allclose(y_hat_cuda, y_true)
-        assert torch.allclose(y_hat_cuda1, y_true)
 
 import torch.utils.benchmark as benchmark
 
@@ -40,7 +38,7 @@ torch.manual_seed(40)
 x = torch.randn(100, 30000, dtype=torch.float32, device=torch.device("cuda:0"))
 
 
-out1 = entmax_nsect_cuda1(x, 1.75, 50, 2)
+out1 = entmax_nsect_cuda(x, 1.75, 50, 2)
 out2 = entmax_nsect(x, 1.75, 50, 2)
 out3 = entmax_nsect_cpp(x, 1.75, 50, 2)
 outtrue = entmax_bisect(x, 1.75)
