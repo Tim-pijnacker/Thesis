7b2397ae12d0f332f0475da0fd3fe3f9219dcdd3
Tim-pijnacker <tim.pijnacker@hotmail.com>
2023-01-13 14:58:13 +0100

    benchmark voor robolab

--- benchmark.py
+++ benchmark.py
@@ -226,14 +226,11 @@ class benchmarker():
 
 
 def main():
-    import torch
-    print(torch)
-    print(torch.cuda.is_available())
-    # bench = benchmarker(alpha = 1.5, nsct_iter = 5, bisct_iter = 25, n_sections = 32, rows = [10, 100], cols = [100, 1000, 10000])
-    # bench.initialise_bench(threads=[1, 8])
-    # bench.initialise_plot()
-    # bench.compare()
-    # bench.plot()
+    bench = benchmarker(alpha = 1.5, nsct_iter = 5, bisct_iter = 25, n_sections = 32, rows = [10, 100], cols = [100, 1000, 10000])
+    bench.initialise_bench(threads=[1])
+    bench.initialise_plot()
+    bench.compare()
+    bench.plot()
 
 if __name__ == "__main__":
     main()
