d70a658b95bcdcb5657a713466b1d7c206b39e4d
Tim-pijnacker <tim.pijnacker@hotmail.com>
2023-01-17 18:15:11 +0100

    lowdim with always 64 threads

--- cuda/nsection_cuda.cpp
+++ cuda/nsection_cuda.cpp
@@ -42,10 +42,11 @@ torch::Tensor nsection_forward1(
     auto shape = torch::_shape_as_tensor(Z);
     auto d = shape[1].item<int>();
 
-    if (d > 2048) {
-        return entmax_cuda_forward(Z, alpha, nIters, nSections);
+    if (d == 64) {
+        return entmax_cuda_forward_lowdim(Z, alpha, nIters, nSections);
     }
-    return entmax_cuda_forward_lowdim(Z, alpha, nIters, nSections);
+    return entmax_cuda_forward(Z, alpha, nIters, nSections);
+    
 }
 
 torch::Tensor sparsemax_backward(
