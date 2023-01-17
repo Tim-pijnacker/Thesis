7d076149b4c6405a1653aab575222ea9d483c605
Tim-pijnacker <tim.pijnacker@hotmail.com>
2023-01-17 17:30:36 +0100

    comment plot lines

--- benchmark.py
+++ benchmark.py
@@ -228,9 +228,9 @@ class benchmarker():
 def main():
     bench = benchmarker(alpha = 1.5, nsct_iter = 5, bisct_iter = 25, n_sections = 32, rows = [10, 100], cols = [100, 1000, 10000], models=["py", "cpp", "cuda", "cuda1", "bisct", "soft"])
     bench.initialise_bench(threads=[1])
-    bench.initialise_plot()
+    # bench.initialise_plot()
     bench.compare()
-    bench.plot()
+    # bench.plot()
 
 if __name__ == "__main__":
     main()
