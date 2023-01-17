361689e8efb272000a93f7dec56fc687cb286d5f
Tim-pijnacker <tim.pijnacker@hotmail.com>
2023-01-17 21:12:26 +0100

    extra benchmark inputs

--- benchmark.py
+++ benchmark.py
@@ -229,7 +229,7 @@ def main():
     bench = benchmarker(alpha = 1.5, nsct_iter = 5, bisct_iter = 25, n_sections = 32, rows = [10, 100], cols = [100, 1000, 10000], models=["py", "cpp", "cuda", "cuda1", "bisct", "soft"])
     bench.initialise_bench(threads=[1])
     bench.compare()
-    bench = benchmarker(alpha = 1.5, nsct_iter = 5, bisct_iter = 25, n_sections = 32, rows = [100, 400], cols = [32000], models=["py", "cpp", "cuda", "cuda1", "bisct", "soft"])
+    bench = benchmarker(alpha = 1.5, nsct_iter = 5, bisct_iter = 25, n_sections = 32, rows = [100, 400, 1000], cols = [32000], models=["py", "cpp", "cuda", "cuda1", "bisct", "soft"])
     bench.initialise_bench(threads=[1])
     # bench.initialise_plot()
     bench.compare()
