9b08346cfb2b09f11d6233a21cb0c6cf8e849be9
tim-pijnacker <tim.pijnacker@hotmail.com>
2023-01-17 23:58:05 +0100

    savefig

--- benchmark.py
+++ benchmark.py
@@ -231,7 +231,7 @@ class benchmarker():
 
 
 def main():
-    # bench = benchmarker(alpha = 1.5, nsct_iter = 5, bisct_iter = 25, n_sections = 32, rows = [10, 100], cols = [100, 1000, 10000], models=["py", "cpp", "cuda1", "bisct", "soft"])
+    bench = benchmarker(alpha = 1.5, nsct_iter = 5, bisct_iter = 25, n_sections = 32, rows = [10, 100], cols = [100, 1000, 10000], models=["py", "cpp", "cuda1", "bisct", "soft"])
     # bench.initialise_bench(threads=[1])
     # bench.compare()
     # bench = benchmarker(alpha = 1.5, nsct_iter = 5, bisct_iter = 25, n_sections = 32, rows = [100, 400], cols = [32000], models=["py", "cpp", "cuda", "cuda1", "bisct", "soft"])
