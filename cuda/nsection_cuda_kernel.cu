f14ad944cf73cfe388e1678a360153a6f1f5ea9a
Tim-pijnacker <tim.pijnacker@hotmail.com>
2023-01-17 20:36:39 +0100

    128, 32

--- cuda/nsection_cuda_kernel.cu
+++ cuda/nsection_cuda_kernel.cu
@@ -266,7 +266,7 @@ torch::Tensor entmax_cuda_forward(
     
 
     int threadsP = 128;
-    int threadsSum = 128;
+    int threadsSum = 32;
 
     // int threadsP = 64;
     // int threadsSum = 32; // 64
