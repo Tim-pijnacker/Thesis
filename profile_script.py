4bdec9c473c14622d7315dc0de07bd234d92dfdb
tim-pijnacker <tim.pijnacker@hotmail.com>
2023-01-18 14:24:37 +0100

    script for profiling

--- profile_script.py
+++ profile_script.py
@@ -3,7 +3,7 @@ from cuda.nsection import entmax_nsect_cuda1
 
 
 def main():
-    x = torch.randn(10, 100, device=torch.device("cuda:0"), dtype=torch.float32)
+    x = torch.randn(100, 10000, device=torch.device("cuda:0"), dtype=torch.float32)
     out = entmax_nsect_cuda1(x, alpha = 1.5, n_iter=5, n_sections=32)
     return 0
 
