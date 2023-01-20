590d1fdf5a9c319909ed81b79badcd9fe1339390
Tim-pijnacker <tim.pijnacker@hotmail.com>
2023-01-20 14:48:16 +0100

    profile for n=9, s=4

--- profile_script.py
+++ profile_script.py
@@ -4,7 +4,7 @@ from cuda.nsection import entmax_nsect_cuda1
 
 def main():
     x = torch.randn(100, 10000, device=torch.device("cuda:0"), dtype=torch.float32)
-    out = entmax_nsect_cuda1(x, alpha = 1.5, n_iter=5, n_sections=32)
+    out = entmax_nsect_cuda1(x, alpha = 1.5, n_iter=9, n_sections=4)
     return 0
 
 if __name__ == "__main__":
