bd037e46734390dc2ff12d1188201c7d090bd96f
Tim-pijnacker <tim.pijnacker@hotmail.com>
2023-02-02 12:16:33 +0100

    maximum accuracy

--- benchmark.py
+++ benchmark.py
@@ -252,11 +252,11 @@ def main():
     bench.initialise_bench(threads=[1])
     bench.compare()
 
-    bench = benchmarker(alpha = 1.5, nsct_iter = 12, bisct_iter = 24, n_sections = 4, rows = [20000], cols = [32000], models=["bisct"])
+    bench = benchmarker(alpha = 1.5, nsct_iter = 12, bisct_iter = 24, n_sections = 4, rows = [11000,12000,13000,14000,15000], cols = [32000], models=["bisct"])
     bench.initialise_bench(threads=[1])
     bench.compare()
 
-    bench = benchmarker(alpha = 1.5, nsct_iter = 12, bisct_iter = 24, n_sections = 4, rows = [20000], cols = [32000], models=["cuda"])
+    bench = benchmarker(alpha = 1.5, nsct_iter = 12, bisct_iter = 24, n_sections = 4, rows = [11000,12000,13000,14000,15000], cols = [32000], models=["cuda"])
     bench.initialise_bench(threads=[1])
     bench.compare()
     # bench.initialise_plot()
