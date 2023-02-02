2da5b0d4e2574fc6c7026e6ebdfe025966d28f8d
Tim-pijnacker <tim.pijnacker@hotmail.com>
2023-02-02 11:13:48 +0100

    cross entropy

--- fairseq/train.sh
+++ fairseq/train.sh
@@ -1,7 +1,7 @@
 # CRITERION=sparsemax_exac
 # CRITERION=sparsemax_bisect
-CRITERION=sparsemax_nsect
-# CRITERION=cross_entropy
+# CRITERION=sparsemax_nsect
+CRITERION=cross_entropy
 # CRITERION=entmax15_exact
 # CRITERION=entmax_bisect
 # CRITERION=entmax_nsect
